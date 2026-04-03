c4f820f00bd9b16e9e1fbebfb6d613f948842001
kenzyalonzo <kenryalonzo@gmail.com>
2026-04-03 01:14:45 +0100

    feat: add Hero component to home page and update background color to white

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import { NavBar } from "@/components/NavBar";
+import { Hero } from "@/components/Hero";
 import { SectionWhyChooseUs } from "@/components/SectionWhyChooseUs";
 
 export default function Home() {
   return (
-    <main className="min-h-screen bg-[#f2f4f7] selection:bg-orange-100 selection:text-orange-900 font-sans">
+    <main className="min-h-screen bg-white selection:bg-orange-100 selection:text-orange-900 font-sans">
       <NavBar />
+      <Hero />
       <SectionWhyChooseUs />
     </main>
   );
